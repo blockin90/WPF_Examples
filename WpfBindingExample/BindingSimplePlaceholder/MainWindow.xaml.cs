4310828faa60d9192f4d12dee334773188c67ddc
Ivan Blok <blockin90@gmail.com>
2022-04-29 23:31:18 +0700

    Добавлен пример реализации примитивного placeholder'а

---
+++ WpfBindingExample/BindingSimplePlaceholder/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BindingSimplePlaceholder
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new People();
+        }
+
+        private void textBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var bindExpresion = textBox.GetBindingExpression(TextBox.TextProperty);
+            var source = bindExpresion.ResolvedSource;
+            var propValue = source.GetType().GetProperty(bindExpresion.ResolvedSourcePropertyName).GetValue(source);
+            if(propValue == null) {
+                textBox.Text = String.Empty;
+            }
+        }
+
+        private void textBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            if (String.IsNullOrEmpty(textBox.Text)) {
+                textBox.Text = null;
+            }
+        }
+
+        private void btnShow_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(DataContext.ToString());
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new People();
+        }
+    }
+}
---
+++ WpfBindingExample/BindingSimplePlaceholder/People.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingSimplePlaceholder
+{
+    class People
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public override string ToString()
+        {
+            return $"{LastName} {FirstName}";
+        }
+    }
+}
